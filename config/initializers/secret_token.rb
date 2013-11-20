55022c0ce6a2115e8e8c2049a5783a50b0bfa8ff
theinternauts <cricketw@wearetheinternauts.com>
2013-11-20 15:54:05 -0800

    rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SleepOverflow::Application.config.secret_token = '2300a410c20a224952f90cfee81fc068be919c3644e4bd3cc49cb6a12954f588d580460cddbe70adaea056c1f692a8f3dc27feec624e45158e24888769be63ca'
