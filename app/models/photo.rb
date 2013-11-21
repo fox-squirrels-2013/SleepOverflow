fe01fbd41511efd9295fed94b901a20c7ceac9c0
Apprentice <apprentice@dbc-19.local>
2013-11-20 23:34:29 -0800

    Added Twitter gem to gemfile. Changed Page model to call Twitter and return one or several search return objects with the hashtag "dbcsleeps" Created a method to extract the media URL that was 8 levels inside of the search return object and instantiate the Photo class. Extracted the media URL (Photo URL) and used this method, currently named photo_create, but open to suggestions. Changed Page model to insert Photo from Twitter into DB. Changed environment file to authenticate our app with Twitter. Added app key information to .gitignore

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,19 @@
+require 'twitter'
+
 class Photo < ActiveRecord::Base
   attr_accessible :url, :twitter_created
 
+# We learnt that the twitpic_gem sucks and so all we need is below. Thanks Rao Rao
+
+search_object = $client.search("#dbcsleeps")
+
+def self.photo_create(response)
+  response.attrs[:statuses].each do |tweet_object|
+    url = tweet_object[:entities][:media][0][:media_url_https]
+    ##twitter_created remains nil for now...
+    Photo.create(url: url, twitter_created: nil)
+  end
+end
+
+self.photo_create(search_object)
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,16 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+env_config = YAML.load_file('/Users/apprentice/Desktop/SleepOverflow/config/twitter.yaml')
+
+env_config.each do |key, value|
+  ENV[key] = value
+end
+
+$client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV['TWITTER_KEY']
+  config.consumer_secret     = ENV['TWITTER_SECRET']
+end
+
 # Initialize the rails application
 SleepOverflow::Application.initialize!
