b68b3a966c96c2a53a0bc915632eb8f72d67dd69
Apprentice <apprentice@dbc-19.local>
2013-11-22 11:46:09 -0800

    Removed white-space from top of photo index view. Added Shoulda Matchers Gem, to allow for Shoulda in tests. Added tests for presence and uniqueness validations of Photo.

--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -3,14 +3,9 @@ require 'spec_helper'
 describe Photo do
   # it { should have_many(:items).through(:item_menus) }
   it { should validate_presence_of(:url)}
+  it { should validate_uniqueness_of(:url)}
   it { should validate_presence_of(:created_at)}
-end
 
-describe Item do
-  # it { should have_many(:menus).through(:item_menus) }
-  it { should validate_presence_of(:title)}
-  it { should validate_presence_of(:cost)}
-end
 
   it "should have a unique id" do
     expect{
