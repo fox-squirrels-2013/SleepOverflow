984853149ae706752a2ab719fcceb255277be8b9
Apprentice <apprentice@dbc-19.local>
2013-11-22 11:50:16 -0800

    Added comments around shoulda for Photo model testing.

--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -1,16 +1,15 @@
 require 'spec_helper'
 
 describe Photo do
-  # it { should have_many(:items).through(:item_menus) }
   it { should validate_presence_of(:url)}
   it { should validate_uniqueness_of(:url)}
   it { should validate_presence_of(:created_at)}
 
-
-  it "should have a unique id" do
-    expect{
-    Photo.create(:url => "hello", :created_at => "10/12")
-    }.to change {Photo.count}.by(1)
-  end
+  # Lines 10 to 14 are now covered by line 6
+  # it "should have a unique id" do
+  #   expect{
+  #   Photo.create(:url => "hello", :created_at => "10/12")
+  #   }.to change {Photo.count}.by(1)
+  # end
 
 end
