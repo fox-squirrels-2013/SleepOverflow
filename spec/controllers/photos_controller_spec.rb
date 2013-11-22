0c8710e7b60eacc081c7b1d6001b1bbfefd431ca
Apprentice <apprentice@dbc-19.local>
2013-11-22 12:00:03 -0800

    Added expectation that index view was receiving @photos as an array.

--- spec/controllers/photos_controller_spec.rb
+++ spec/controllers/photos_controller_spec.rb
@@ -2,10 +2,13 @@ require 'spec_helper'
 
 
 describe PhotosController do
-  it '#index gets a 200 get response' do
-    get :index
-    response.status.should eq 200
-  end
+    it '#index gets a 200 get response' do
+      get :index
+      response.status.should eq 200
+      expect(assigns(:photos)).to be_an_instance_of(Array)
+    end
+
 end
+
 
 
