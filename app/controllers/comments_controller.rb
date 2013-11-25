dcab1e2903a5c78b8f627a99ca04922a84da275d
Apprentice <apprentice@dbc-19.local>
2013-11-24 23:49:57 -0800

    Refractored the JS a little, to even up the formatting of ajax generated comments and erb formatted comments.
    Edited CommentController create action in order to respond to blank comment entries.
    
    Stopped non-users from commenting. TOO BAD

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,8 +3,11 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(params[:comment])
     @comment.photo_id = params[:photo_id]
-    @comment.save
+    if @comment.save
     # Having specifed that I wanted to render json, I had to make up a key for the rest of it, hence, bender
       render :json => {bender: @comment }
+    else
+      render :json => {bender: {username: @comment.username, content: "You have to write something to comment, derp"}}
+    end
   end
 end
